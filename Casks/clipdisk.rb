434fb4db27c1dfb78c55324d71b6b4c9bcf38dc4
Mitchell Currie <mitch@mitchellcurrie.com>
2026-04-19 17:44:28 +1000

    clipdisk 1.1.3

--- Casks/clipdisk.rb
+++ Casks/clipdisk.rb
@@ -1,11 +1,11 @@
 cask "clipdisk" do
   version "1.1.3"
-  sha256 "01a5f0401b9745f1c7d0abaebc670e998390e4db935e04350c3d0fda68cef184"
+  sha256 "6f6da62ef304befccb81e3cb5cdd962ba9f90375f319138705809d476aea4abc"
 
-  url "https://github.com/mitchins/clipdisk/releases/download/v#{version}/ClipboardFolder-#{version}.dmg"
+  url "https://github.com/mitchins/clipdisk/releases/download/v#{version}/ClipDisk-#{version}.dmg"
   name "Clipdisk"
   desc "Clipboard contents on a RAM disk for quick file uploads"
   homepage "https://github.com/mitchins/clipdisk"
 
-  app "ClipboardFolder.app"
+  app "ClipDisk.app"
 end
