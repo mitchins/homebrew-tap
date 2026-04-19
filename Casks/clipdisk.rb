905d4f9445c1e4a262d8b6a9eddbe30157224840
Mitchell Currie <mitch@mitchellcurrie.com>
2026-04-19 17:13:57 +1000

    clipdisk 1.1

--- Casks/clipdisk.rb
+++ Casks/clipdisk.rb
@@ -1,6 +1,6 @@
 cask "clipdisk" do
-  version "1.0.6"
-  sha256 "c240449965aaf49001c89c823b67837d9ce8b287f1166eb8aad64260399d0023"
+  version "1.1"
+  sha256 "157657b8d599865e8d17fe3a745e326ee2f6078e4664f577928e129949a953c6"
 
   url "https://github.com/mitchins/clipdisk/releases/download/v#{version}/ClipboardFolder-#{version}.dmg"
   name "Clipdisk"
